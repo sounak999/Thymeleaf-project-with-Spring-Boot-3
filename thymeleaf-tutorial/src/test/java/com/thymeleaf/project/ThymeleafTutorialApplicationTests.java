1be57ae8b09f5aab078ea6a7610cc02acedb0214
Sounak <sounaksaha333@gmail.com>
2025-02-12 23:46:55 +0530

    0.1 => intial project files

---
+++ thymeleaf-tutorial/src/test/java/com/thymeleaf/project/ThymeleafTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thymeleaf.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
