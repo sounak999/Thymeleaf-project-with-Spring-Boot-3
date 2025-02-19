0d181d9cf6522fbbbc998bcceb76ba7d41e21909
Sounak <sounaksaha333@gmail.com>
2025-02-20 03:11:41 +0530

    0.3 => form submit and open a new endpoint to show data

--- thymeleaf-tutorial/src/main/java/com/thymeleaf/project/controller/FormController.java
+++ thymeleaf-tutorial/src/main/java/com/thymeleaf/project/controller/FormController.java
@@ -4,6 +4,8 @@ import com.thymeleaf.project.model.UserForm;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,5 +22,12 @@ public class FormController {
         List<String> listProfession = Arrays.asList("Developer", "Tester", "Architect");
         model.addAttribute("listProfession", listProfession);
         return "register-form";
+    }
+
+    // handler to submit request
+    @PostMapping("/register/save")
+    public String submitForm(Model model, @ModelAttribute("userForm") UserForm userForm) {
+        model.addAttribute("userForm", userForm);
+        return "register-success";
     }
 }
